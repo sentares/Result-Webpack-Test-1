13c1caae7310c85a937dd5f56da1723f9ce3d5e6
sentares <aydararzybekov2002@mail.ru>
2025-04-14 14:21:31 +0600

    chore:add HtmlWebpackPlugin and update dev/prod scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	entry: './index.js',
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'public/index.html'),
+		}),
+	],
 }

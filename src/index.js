e02c193ef173f00342711fdc8b4cb7662ac9916f
sentares <aydararzybekov2002@mail.ru>
2025-04-15 09:56:52 +0600

    feat:add change bg logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './index.scss'
 
-console.log('hello')
+let activeWeather = {
+	audio: null,
+	currentSound: null,
+	bgImage: null,
+}
+
+window.handleClickButton = function () {
+	const button = event.currentTarget
+	const bgImage = button.getAttribute('data-bg')
+
+	activeWeather.bgImage = bgImage
+
+	document.body.style.backgroundImage = `url('./img/${bgImage}')`
+}

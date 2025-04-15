cc1e4062246f09029f74b967d69b07a4e7f3552b
sentares <aydararzybekov2002@mail.ru>
2025-04-15 14:39:37 +0600

    chore:initializing eslint

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
-import './index.scss'
+import './index.scss';
 
-let activeWeather = {
-	audio: null,
-	currentSound: null,
-	bgImage: null,
-}
+const activeWeather = {
+  audio: null,
+  currentSound: null,
+  bgImage: null,
+};
 
-window.handleClickButton = function () {
-	const button = event.currentTarget
-	const bgImage = button.getAttribute('data-bg')
-	const soundFile = button.getAttribute('data-sound')
+window.handleClickButton = function (event) {
+  const button = event.currentTarget;
+  const bgImage = button.getAttribute('data-bg');
+  const soundFile = button.getAttribute('data-sound');
 
-	activeWeather.bgImage = bgImage
-	activeWeather.soundFile = soundFile
+  activeWeather.bgImage = bgImage;
+  activeWeather.soundFile = soundFile;
 
-	document.body.style.backgroundImage = `url('./img/${bgImage}')`
+  document.body.style.backgroundImage = `url('./img/${bgImage}')`;
 
-	if (activeWeather.currentSound === soundFile) {
-		if (activeWeather.audio.paused) {
-			activeWeather.audio.play()
-		} else {
-			activeWeather.audio.pause()
-		}
-	} else {
-		if (activeWeather.audio) {
-			activeWeather.audio.pause()
-			activeWeather.audio.currentTime = 0
-		}
+  if (activeWeather.currentSound === soundFile) {
+    if (activeWeather.audio.paused) {
+      activeWeather.audio.play();
+    } else {
+      activeWeather.audio.pause();
+    }
+  } else {
+    if (activeWeather.audio) {
+      activeWeather.audio.pause();
+      activeWeather.audio.currentTime = 0;
+    }
 
-		activeWeather.audio = new Audio(`./sounds/${soundFile}`)
+    activeWeather.audio = new Audio(`./sounds/${soundFile}`);
 
-		activeWeather.audio.loop = true
-		activeWeather.audio.play()
-		activeWeather.currentSound = soundFile
-	}
-}
+    activeWeather.audio.loop = true;
+    activeWeather.audio.play();
+    activeWeather.currentSound = soundFile;
+  }
+};
 
 document.getElementById('volume').addEventListener('input', function () {
-	if (activeWeather.audio) {
-		activeWeather.audio.volume = this.value
-	}
-})
+  if (activeWeather.audio) {
+    activeWeather.audio.volume = this.value;
+  }
+});

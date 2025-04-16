84837eebfabcc5dac51104348f65395d270f938d
sentares <aydararzybekov2002@mail.ru>
2025-04-16 16:30:30 +0600

    chore:add ts-loader and upfate webpack config

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -11,6 +11,9 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.jsx'],
+	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, 'public/index.html'),
@@ -54,6 +57,11 @@ module.exports = {
 			{
 				test: /\.(mp3|wav)$/,
 				type: 'asset/resource',
+			},
+			{
+				test: /\.[tj]sx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
 			},
 		],
 	},

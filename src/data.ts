e264eacb163ba2ef89ad181aaf841801a5e028b1
sentares <aydararzybekov2002@mail.ru>
2025-04-16 18:23:08 +0600

    fix(data.ts):change file to ts and add types

--- src/data.ts
+++ src/data.ts
@@ -1,17 +1,25 @@
-export default [
-	{
-		name: 'summer',
-		audioName: 'summer.mp3',
-		imgName: 'summer-bg.jpg',
-	},
-	{
-		name: 'rainy',
-		audioName: 'rain.mp3',
-		imgName: 'rainy-bg.jpg',
-	},
-	{
-		name: 'winter',
-		audioName: 'winter.mp3',
-		imgName: 'winter-bg.jpg',
-	},
-]
+export interface IWeatherItem {
+	name: string
+	imgName: string
+	audioName: string
+}
+
+const WeatherData: IWeatherItem[] = [
+  {
+    name: 'summer',
+    audioName: 'summer.mp3',
+    imgName: 'summer-bg.jpg',
+  },
+  {
+    name: 'rainy',
+    audioName: 'rain.mp3',
+    imgName: 'rainy-bg.jpg',
+  },
+  {
+    name: 'winter',
+    audioName: 'winter.mp3',
+    imgName: 'winter-bg.jpg',
+  },
+];
+
+export default WeatherData;

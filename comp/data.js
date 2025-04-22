caec8aa5ffb716a2b93b31cb0c73c18eeb47f1b3
sentares <aydararzybekov2002@mail.ru>
2025-04-22 15:38:44 +0600

    fix:update interface of WeatherItem and change logic of source media
    1. rename interface IWeatherItem to WeatherItem
    2. add imports of media files and update values of keys items in arr of WeatherItems
    3. update logic of changing bg and sound

--- comp/data.js
+++ comp/data.js
@@ -1,20 +1,31 @@
 'use strict'
+var __importDefault =
+	(this && this.__importDefault) ||
+	function (mod) {
+		return mod && mod.__esModule ? mod : { default: mod }
+	}
 Object.defineProperty(exports, '__esModule', { value: true })
+const summer_bg_jpg_1 = __importDefault(require('../public/img/summer-bg.jpg'))
+const rainy_bg_jpg_1 = __importDefault(require('../public/img/rainy-bg.jpg'))
+const winter_bg_jpg_1 = __importDefault(require('../public/img/winter-bg.jpg'))
+const summer_mp3_1 = __importDefault(require('../public/sounds/summer.mp3'))
+const rain_mp3_1 = __importDefault(require('../public/sounds/rain.mp3'))
+const winter_mp3_1 = __importDefault(require('../public/sounds/winter.mp3'))
 const WeatherData = [
 	{
 		name: 'summer',
-		audioName: 'summer.mp3',
-		imgName: 'summer-bg.jpg',
+		audioName: summer_mp3_1.default,
+		imgName: summer_bg_jpg_1.default,
 	},
 	{
 		name: 'rainy',
-		audioName: 'rain.mp3',
-		imgName: 'rainy-bg.jpg',
+		audioName: rain_mp3_1.default,
+		imgName: rainy_bg_jpg_1.default,
 	},
 	{
 		name: 'winter',
-		audioName: 'winter.mp3',
-		imgName: 'winter-bg.jpg',
+		audioName: winter_mp3_1.default,
+		imgName: winter_bg_jpg_1.default,
 	},
 ]
 exports.default = WeatherData
